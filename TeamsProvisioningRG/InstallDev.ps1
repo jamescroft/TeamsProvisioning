ee8dfe92208a83c481bd0aeefdbc5450055e1252
James Croft <james.croft@gmail.com>
2020-05-26 23:09:39 +1000

    initial commit

--- TeamsProvisioningRG/InstallDev.ps1
+++ TeamsProvisioningRG/InstallDev.ps1
@@ -5,12 +5,12 @@
     -TemplateParametersFile dev.parameters.json
 
 # Ensure storage queues are in place
-$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "TeamsProvisioningDev"
+$storageAccount = Get-AzStorageAccount | Where-Object ResourceGroupName -eq "TeamsProvisioningDev"
 $storageContext = $storageAccount.Context
-New-AzureStorageQueue -Name "create-team-request-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "create-team-completion-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "clone-team-request-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "clone-team-completion-queue" -Context $storageContext -ErrorAction Ignore
+New-AzStorageQueue -Name "create-team-request-queue" -Context $storageContext -ErrorAction Ignore
+New-AzStorageQueue -Name "create-team-completion-queue" -Context $storageContext -ErrorAction Ignore
+New-AzStorageQueue -Name "clone-team-request-queue" -Context $storageContext -ErrorAction Ignore
+New-AzStorageQueue -Name "clone-team-completion-queue" -Context $storageContext -ErrorAction Ignore
 
 
 
